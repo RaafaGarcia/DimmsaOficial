b28a33b9271b2ce64877d6e9790e0fd05663e9bb
SrLezhuga <58528751+SrLezhuga@users.noreply.github.com>
2019-12-16 00:38:05 -0600

    Update mail.php

--- Controlador/mail.php
+++ Controlador/mail.php
@@ -1,10 +1,5 @@
 <?php
 $para      = 'brihand.lech@gmail.com';
-$titulo    = 'El título';
-$mensaje   = 'Hola';
-$cabeceras = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
 
 mail($para, $titulo, $mensaje, $cabeceras);
 ?>
