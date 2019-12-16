c8ba397cf30955933e2855026f13b54b7c60101c
SrLezhuga <58528751+SrLezhuga@users.noreply.github.com>
2019-12-16 00:38:21 -0600

    Update mail.php

--- Controlador/mail.php
+++ Controlador/mail.php
@@ -1,5 +1,11 @@
 <?php
+$Nombre = $_POST['name'];
+$correo = $_POST['email'];
+
 $para      = 'brihand.lech@gmail.com';
+$titulo = $_POST['Subject'];
+$mensaje = 'Correo de: '.$correo.'/r/n'.$_POST['text'];
+$cabeceras = 'From: '.$correo;
 
 mail($para, $titulo, $mensaje, $cabeceras);
 ?>
